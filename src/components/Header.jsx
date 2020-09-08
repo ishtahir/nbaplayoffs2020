1e070e29a2ff7482ff7b14ee5a601c568ba38fe2
Ish Tahir <tahirismaeel@gmail.com>
2020-09-08 18:55:48 -0500

    Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <header className='header'>
+    <h1 className='title'>
+      <span className='text text-nba'>NBA</span>
+      <span className='text text-playoffs'>Playoffs</span>
+      <span className='text text-2020'>2020</span>
+      <span className='text-wng'>#WholeNewGame</span>
+    </h1>
+  </header>
+);
+
+export default Header;
