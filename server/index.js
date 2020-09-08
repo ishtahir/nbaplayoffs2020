2b5297b3f7942da797b83b26710f70991e0d105c
Ish Tahir <tahirismaeel@gmail.com>
2020-09-08 18:37:59 -0500

    Created /load route to send data to client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const fs = require('fs');
+const { getAllScores } = require('./getData.js');
 
 const app = express();
+
+app.get('/load', (req, res) => {
+  const playoffs = JSON.parse(
+    fs.readFileSync('server/files/playoffs-round1.json', 'utf8')
+  );
+  res.send(playoffs);
+});
+
+// app.get('/scores/rd/:num', (req, res) => {
+//   const round = +req.params.num;
+//   if (typeof round === 'number') {
+//     getAllScores('west', round);
+//     getAllScores('east', round);
+//   }
+//   res.send('Thank you.');
+// });
 
 const port = process.env.PORT || 4501;
 
