22e14b2da04e9c4467b859d1f02baca5f9705ea6
Ish Tahir <tahirismaeel@gmail.com>
2020-09-08 21:15:54 -0500

    Added markup for team images

--- src/components/Matchup.jsx
+++ src/components/Matchup.jsx
@@ -6,15 +6,33 @@ const Matchup = ({
   lowSeed,
   highSeedWins,
   lowSeedWins,
+  highSeedImg,
+  lowSeedImg,
+  highSeedMascot,
+  lowSeedMascot,
 }) => (
   <div className='matchup'>
     <ul className={conference}>
       <li>
         <span className='seed-left seed'>{highSeed}</span>
+        <span className='team'>
+          <img
+            src={`images/${highSeedImg}`}
+            className={`${highSeedMascot}-img`}
+            alt=''
+          />
+        </span>
         <span className='wins'>{highSeedWins}</span>
       </li>
       <li>
         <span className='seed-right seed'>{lowSeed}</span>
+        <span className='team'>
+          <img
+            src={`images/${lowSeedImg}`}
+            className={`${lowSeedMascot}-img`}
+            alt=''
+          />
+        </span>
         <span className='wins'>{lowSeedWins}</span>
       </li>
     </ul>
