349b579df679cffae14ea0baf0a0d7c1bb9fa4d7
Ish Tahir <tahirismaeel@gmail.com>
2020-09-08 21:24:52 -0500

    Seperated logic for each round into its own component

--- src/components/Bracket.jsx
+++ src/components/Bracket.jsx
@@ -1,126 +1,16 @@
 import React from 'react';
-import Matchup from './Matchup.jsx';
+import Round1 from './Round1.jsx';
+import Round2 from './Round2.jsx';
+import Round3 from './Round3.jsx';
+import Round4 from './Round4.jsx';
 
 const Bracket = () => {
   return (
     <div className='bracket'>
-      <div className='col rd1'>
-        <Matchup
-          conference='west'
-          highSeed={1}
-          lowSeed={8}
-          highSeedWins={0}
-          lowSeedWins={0}
-          highSeedImg='LAL.svg'
-          lowSeedImg='POR.svg'
-          highSeedMascot='Lakers'
-          lowSeedMascot='Trailblazers'
-        />
-        <Matchup
-          conference='west'
-          highSeed={4}
-          lowSeed={5}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='west'
-          highSeed={2}
-          lowSeed={7}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='west'
-          highSeed={3}
-          lowSeed={6}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='east'
-          highSeed={1}
-          lowSeed={8}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='east'
-          highSeed={4}
-          lowSeed={5}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='east'
-          highSeed={2}
-          lowSeed={7}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='east'
-          highSeed={3}
-          lowSeed={6}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-      </div>
-      <div className='col rd2'>
-        <Matchup
-          conference='west'
-          highSeed={1}
-          lowSeed={4}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='west'
-          highSeed={2}
-          lowSeed={3}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='east'
-          highSeed={1}
-          lowSeed={4}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='east'
-          highSeed={2}
-          lowSeed={3}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-      </div>
-      <div className='col rd3'>
-        <Matchup
-          conference='west'
-          highSeed={1}
-          lowSeed={2}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-        <Matchup
-          conference='east'
-          highSeed={1}
-          lowSeed={2}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-      </div>
-      <div className='col rd4'>
-        <Matchup
-          conference='finals'
-          highSeed={1}
-          lowSeed={1}
-          highSeedWins={0}
-          lowSeedWins={0}
-        />
-      </div>
+      <Round1 />
+      <Round2 />
+      <Round3 />
+      <Round4 />
     </div>
   );
 };
