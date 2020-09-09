de0c921ddc8ff21611fd37e20e3c5ed104a135a4
Ish Tahir <tahirismaeel@gmail.com>
2020-09-08 21:05:34 -0500

    Put className on ul instead of li

--- src/components/Matchup.jsx
+++ src/components/Matchup.jsx
@@ -8,12 +8,12 @@ const Matchup = ({
   lowSeedWins,
 }) => (
   <div className='matchup'>
-    <ul>
-      <li className={conference}>
+    <ul className={conference}>
+      <li>
         <span className='seed-left seed'>{highSeed}</span>
         <span className='wins'>{highSeedWins}</span>
       </li>
-      <li className={conference}>
+      <li>
         <span className='seed-right seed'>{lowSeed}</span>
         <span className='wins'>{lowSeedWins}</span>
       </li>
