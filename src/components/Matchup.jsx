ca0ab61f75d28020deb2d0e65a9381b3aaa7e689
Ish Tahir <tahirismaeel@gmail.com>
2020-09-08 21:39:10 -0500

    Updated to use the Team component

--- src/components/Matchup.jsx
+++ src/components/Matchup.jsx
@@ -1,40 +1,11 @@
 import React from 'react';
+import Team from './Team.jsx';
 
-const Matchup = ({
-  conference,
-  highSeed,
-  lowSeed,
-  highSeedWins,
-  lowSeedWins,
-  highSeedImg,
-  lowSeedImg,
-  highSeedMascot,
-  lowSeedMascot,
-}) => (
+const Matchup = ({ conference, seed, wins, img, mascot }) => (
   <div className='matchup'>
     <ul className={conference}>
-      <li>
-        <span className='seed-left seed'>{highSeed}</span>
-        <span className='team'>
-          <img
-            src={`images/${highSeedImg}`}
-            className={`${highSeedMascot}-img`}
-            alt=''
-          />
-        </span>
-        <span className='wins'>{highSeedWins}</span>
-      </li>
-      <li>
-        <span className='seed-right seed'>{lowSeed}</span>
-        <span className='team'>
-          <img
-            src={`images/${lowSeedImg}`}
-            className={`${lowSeedMascot}-img`}
-            alt=''
-          />
-        </span>
-        <span className='wins'>{lowSeedWins}</span>
-      </li>
+      <Team seed={seed} img={img} mascot={mascot} wins={wins} high={true} />
+      <Team seed={seed} img={img} mascot={mascot} wins={wins} high={false} />
     </ul>
   </div>
 );
