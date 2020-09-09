321e124f988cda0bdddcda43d1507f54663299bb
Ish Tahir <tahirismaeel@gmail.com>
2020-09-09 00:55:37 -0500

    Added middleware
    - Installed cors and applied it
    - Using express.static() and express.json()

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
 const { getAllScores } = require('./getData.js');
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('dist'));
 
 app.get('/load', (req, res) => {
   const playoffs = JSON.parse(
