e74b59170692f87a0b13068de713d70f5decfdad
Ish Tahir <tahirismaeel@gmail.com>
2020-09-09 02:42:07 -0500

    Updated 4 Round component files to one Round component file
    - Passing down round number and matchups for that round

---
+++ src/components/Round.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Matchup from './Matchup.jsx';
+
+const Round = ({ matchups, round }) => {
+  return (
+    <div className={`col rd${round}`}>
+      {matchups.map(({ conf, seriesName, highSeed, lowSeed }, i) => (
+        <Matchup
+          key={`${seriesName}${i}`}
+          conference={conf}
+          highSeed={highSeed.seed}
+          lowSeed={lowSeed.seed}
+          highWins={highSeed.wins}
+          lowWins={lowSeed.wins}
+          highImg={`${highSeed.short}.svg`}
+          lowImg={`${lowSeed.short}.svg`}
+          highMascot={highSeed.mascot}
+          lowMascot={lowSeed.mascot}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Round;
