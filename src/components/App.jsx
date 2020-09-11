3f5c81823563d8228e35949d4d138a125ae7a43a
Ish Tahir <tahirismaeel@gmail.com>
2020-09-11 15:32:29 -0500

    Added state to keep track of seriesClicked
    - Passing down the setSeriesClicked function to Matchup component
    - If a series is clicked placeholder text showing up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header.jsx';
 import Bracket from './Bracket.jsx';
 
 const App = () => {
+  const [seriesClicked, setSeriesClicked] = useState('');
+
   return (
     <>
       <Header />
-      <Bracket />
+      {seriesClicked === '' ? (
+        <Bracket setSeriesClicked={setSeriesClicked} />
+      ) : (
+        <p>Series Table coming soon...</p>
+      )}
     </>
   );
 };
