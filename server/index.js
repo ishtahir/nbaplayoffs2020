965ee7d7bf47fe29085bf4cced256548e3c4cd6c
Ish Tahir <tahirismaeel@gmail.com>
2020-09-11 01:29:39 -0500

    Made the following changes:
    - Importing schema, and mongoose files
    - Set up connection to Mongo Atlas
    - Set up 2 get routes to get an individual series, or all series for a
    specific round
    - A post route, to add a new series
    - A patch route to update an existing series
    - A delete route to delete a series

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
-const { getAllScores } = require('./getData.js');
-const { start } = require('./settings.js');
+const mongoose = require('mongoose');
+
+const nbaSeries = require('./series.js');
+const { mongoPassword } = require('./config.js');
+const allSeries = JSON.parse(
+  fs.readFileSync('server/files/playoffs-all-series-2020.json')
+);
 
 const app = express();
 
@@ -10,35 +15,109 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('dist'));
 
-app.get('/load/rd/:num', (req, res) => {
-  const round = req.params.num;
-  const playoffs = JSON.parse(
-    fs.readFileSync(`server/files/playoffs-round${round}.json`, 'utf8')
+mongoose.connect(
+  `mongodb+srv://ish:${mongoPassword}@cluster0.acvuo.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.get('/series/:name', (req, res) => {
+  nbaSeries
+    .findOne({ seriesName: req.params.name })
+    .exec()
+    .then((result) => {
+      console.log('Success getting document!');
+      res.status(200).json(result);
+    })
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ error: err, message: 'Could not properly get document!' })
+    );
+});
+
+app.get('/series/rd/:rd', (req, res) => {
+  nbaSeries
+    .find({ round: +req.params.rd })
+    .exec()
+    .then((result) => {
+      console.log('Success getting documents!');
+      res.status(200).json(result);
+    })
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ error: err, message: 'Could not properly get documents!' })
+    );
+});
+
+app.post('/series/:name', (req, res) => {
+  const matchup = allSeries.find(
+    (match) => match.seriesName === req.params.name
   );
-  res.send(playoffs);
+
+  const series = new nbaSeries(matchup);
+  nbaSeries
+    .findOne({ seriesName: matchup.seriesName })
+    .exec()
+    .then((result) => {
+      if (result === null) {
+        series
+          .save()
+          .then((_) => {
+            res.status(200).json({ message: 'Success creating document!' });
+          })
+          .catch((err) =>
+            res.status(500).json({
+              error: err,
+              message: 'Could not properly create document!',
+            })
+          );
+      } else {
+        res.status(200).json({ message: 'Document already exists!' });
+      }
+    });
 });
 
-app.get('/create/rd/:num', (req, res) => {
-  const round = +req.params.num;
-  start(round);
-  res.redirect('/');
+app.patch('/series/:name', (req, res) => {
+  const matchup = allSeries.find(
+    (match) => match.seriesName === req.params.name
+  );
+  const games = matchup.games;
+  nbaSeries
+    .updateOne({ seriesName: req.params.name }, { $set: { games: games } })
+    .exec()
+    .then((result) => {
+      console.log('Success updating document!');
+      res.status(200).json({
+        message: `Games successfully updated for ${matchup.seriesName}`,
+        res: result,
+      });
+    })
+    .catch((err) =>
+      res.status(500).json({
+        error: err,
+        message: 'Could not properly update document!',
+      })
+    );
 });
 
-app.get('/scores/:conf/:num', async (req, res) => {
-  const conf = req.params.conf;
-  const round = +req.params.num;
-  await getAllScores(conf, round);
-  res.redirect(`/load/rd/${round}`);
+app.delete('/series/:name', (req, res) => {
+  nbaSeries
+    .remove({ seriesName: req.params.name })
+    .exec()
+    .then((result) =>
+      res.status(200).json({ message: `${req.params.name} has been deleted!` })
+    )
+    .catch((err) =>
+      res.status(500).json({
+        error: err,
+        message: 'Could not properly delete document!',
+      })
+    );
 });
-
-// app.get('/scores/rd/:num', (req, res) => {
-//   const round = +req.params.num;
-//   if (typeof round === 'number') {
-//     getAllScores('west', round);
-//     getAllScores('east', round);
-//   }
-//   res.send('Thank you.');
-// });
 
 const port = process.env.PORT || 4501;
 
