801cb0e5ee6e1d850a7f3cfef2347dd717b45481
Ish Tahir <tahirismaeel@gmail.com>
2020-09-11 16:32:07 -0500

    Passed down setSeries function

--- src/components/Round.jsx
+++ src/components/Round.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import Matchup from './Matchup.jsx';
 
-const Round = ({ matchups, round, setSeriesClicked }) => {
+const Round = ({ matchups, round, setSeriesClicked, setSeries }) => {
   return (
     <div className={`col rd${round}`}>
-      {matchups.map(({ conf, seriesName, highSeed, lowSeed }, i) => (
+      {matchups.map((matchup, i) => (
         <Matchup
-          key={`${seriesName}${i}`}
-          conference={highSeed.conf}
-          highSeed={highSeed.seed}
-          lowSeed={lowSeed.seed}
-          highWins={highSeed.wins}
-          lowWins={lowSeed.wins}
-          highImg={`${highSeed.short}.svg`}
-          lowImg={`${lowSeed.short}.svg`}
-          highMascot={highSeed.mascot}
-          lowMascot={lowSeed.mascot}
-          seriesName={seriesName}
+          key={`${matchup.seriesName}${i}`}
+          conference={matchup.highSeed.conf}
+          highSeed={matchup.highSeed.seed}
+          lowSeed={matchup.lowSeed.seed}
+          highWins={matchup.highSeed.wins}
+          lowWins={matchup.lowSeed.wins}
+          highImg={`${matchup.highSeed.short}.svg`}
+          lowImg={`${matchup.lowSeed.short}.svg`}
+          highMascot={matchup.highSeed.mascot}
+          lowMascot={matchup.lowSeed.mascot}
+          seriesName={matchup.seriesName}
           setSeriesClicked={setSeriesClicked}
+          setSeries={setSeries}
+          series={matchup}
         />
       ))}
     </div>
