a22a99524695221f0993c9fa4457f6f306af93a9
Ish Tahir <tahirismaeel@gmail.com>
2020-09-11 15:10:05 -0500

    Rendering Nav component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Nav from './Nav.jsx';
 
 const Header = () => (
-  <header className='header'>
-    <h1 className='title'>
-      <span className='text text-nba'>NBA</span>
-      <span className='text text-playoffs'>Playoffs</span>
-      <span className='text text-2020'>2020</span>
-      <span className='text-wng'>#WholeNewGame</span>
-    </h1>
-  </header>
+  <>
+    <header className='header'>
+      <h1 className='title'>
+        <span className='text text-nba'>NBA</span>
+        <span className='text text-playoffs'>Playoffs</span>
+        <span className='text text-2020'>2020</span>
+        <span className='text-wng'>#WholeNewGame</span>
+      </h1>
+    </header>
+    <Nav />
+  </>
 );
 
 export default Header;
