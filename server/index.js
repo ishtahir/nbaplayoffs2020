a74687ab80bcb0ea16dca91dad6a380c7e4ae493
Ish Tahir <tahirismaeel@gmail.com>
2020-09-13 13:53:28 -0500

    Accessing process vars to heroku

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const nbaSeries = require('./series.js');
-const { mongoPassword, databaseName } = require('./config.js');
+const { mongoPassword, databaseName } = process.env || require('./config.js');
+
 const allSeries = JSON.parse(
   fs.readFileSync('server/files/playoffs-all-series-2020.json')
 );
@@ -43,13 +44,13 @@ app.get('/series/', (req, res) => {
     .find({})
     .exec()
     .then((result) => {
-      console.log('Success getting documents!');
+      console.log(`Success getting all documents`);
       res.status(200).json(result);
     })
     .catch((err) =>
       res
         .status(500)
-        .json({ error: err, message: 'Could not properly get documents!' })
+        .json({ error: err, message: `Error getting all documents` })
     );
 });
 
@@ -67,42 +68,46 @@ app.post('/series/:name', (req, res) => {
         series
           .save()
           .then((_) => {
-            res.status(200).json({ message: 'Success creating document!' });
+            res.status(200).json({
+              message: `Success creating document: ${req.params.name}`,
+            });
           })
           .catch((err) =>
             res.status(500).json({
               error: err,
-              message: 'Could not properly create document!',
+              message: `Error creating document: ${req.params.name}`,
             })
           );
       } else {
-        res.status(200).json({ message: 'Document already exists!' });
+        res
+          .status(200)
+          .json({ message: `Document already exists: ${req.params.name}` });
       }
     });
 });
 
 app.patch('/series/:name', (req, res) => {
-  const matchup = allSeries.find(
-    (match) => match.seriesName === req.params.name
-  );
-  const changesObj = {};
-  for (const change of req.body) {
-    changesObj[change] = matchup[change];
-  }
+  // const matchup = allSeries.find(
+  //   (match) => match.seriesName === req.params.name
+  // );
+  // const changesObj = {};
+  // for (const change of req.body) {
+  //   changesObj[change] = matchup[change];
+  // }
+  // console.log(changesObj);
   nbaSeries
-    .updateOne({ seriesName: req.params.name }, { $set: changesObj })
+    .updateOne({ seriesName: req.params.name }, { $set: req.body })
     .exec()
-    .then((result) => {
-      console.log('Success updating document!');
+    .then((result) =>
       res.status(200).json({
-        message: `Games successfully updated for ${matchup.seriesName}`,
+        message: `Success updating document: ${req.params.name}`,
         res: result,
-      });
-    })
+      })
+    )
     .catch((err) =>
       res.status(500).json({
         error: err,
-        message: 'Could not properly update document!',
+        message: `Error updating document: ${req.params.name}`,
       })
     );
 });
@@ -112,12 +117,15 @@ app.delete('/series/:name', (req, res) => {
     .remove({ seriesName: req.params.name })
     .exec()
     .then((result) =>
-      res.status(200).json({ message: `${req.params.name} has been deleted!` })
+      res.status(200).json({
+        message: `Success deleting document: ${req.params.name}`,
+        res: result,
+      })
     )
     .catch((err) =>
       res.status(500).json({
         error: err,
-        message: 'Could not properly delete document!',
+        message: `Error deleting document: ${req.params.name}`,
       })
     );
 });
