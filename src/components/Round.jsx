5fe7d779426ce0c19de99565bf4b395e71dd1bae
Ish Tahir <tahirismaeel@gmail.com>
2020-09-14 00:25:36 -0500

    Added nav-item for each round column

--- src/components/Round.jsx
+++ src/components/Round.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 import Matchup from './Matchup.jsx';
 
 const Round = ({ matchups, round, setSeriesClicked, setSeries }) => {
+  function navText() {
+    let text = '';
+    if (round === 1) {
+      text += 'Round 1';
+    } else if (round === 2) {
+      text += 'Conf. Semi-Finals';
+    } else if (round === 3) {
+      text += 'Conference Finals';
+    } else {
+      text += 'NBA Finals';
+    }
+    return text;
+  }
   return (
     <div className={`col rd${round}`}>
+      <span className='nav-item'>{navText()}</span>
       {matchups.map((matchup, i) => (
         <Matchup
           key={`${matchup.seriesName}${i}`}
