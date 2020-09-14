c22d611dbae4fdff89714b8dd9ba07d880c53070
Ish Tahir <tahirismaeel@gmail.com>
2020-09-14 00:58:20 -0500

    Added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className='footer'>
+    <span className='footer-reg'>&copy; 2020 All Rights Reserved.</span>
+    <br />
+    <span className='footer-reg'>Made with ❤️ by Ish Tahir.</span>
+    <br />
+    <span className='footer-small'>
+      All images are the exclusive rights of the NBA.
+    </span>
+  </footer>
+);
+
+export default Footer;
